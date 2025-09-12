4141a81bc58c9e9c4f7577f831fb9645f8e0cca6
Cu pid <kelanidarasimi9@gmail.com>
2025-09-12 06:20:19 +0100

    chore: create task table

--- repository/postgres/connect.go
+++ repository/postgres/connect.go
@@ -11,39 +11,27 @@ import (
 )
 
 type PostgresConn struct {
-	Port     int
-	Password string
-	Host     string
-	Database string
-	User     string
-	Uri      string
-	SSLMode  string
+	Conn *pgx.Conn
 }
 
-func NewPostgresConn(uri, password, port, host, database, user, sslmode string) (*PostgresConn, error) {
+func NewPostgresConn(conn *pgx.Conn) *PostgresConn {
+	return &PostgresConn{Conn: conn}
+}
+
+func ConnectPostgres(uri, password, port, host, database, user, sslmode string) (*PostgresConn, error) {
 	portInt, err := strconv.Atoi(port)
 	if err != nil {
 		log.Println(err.Error())
 		return nil, err
 	}
-	return &PostgresConn{Port: portInt,
-		Password: password,
-		Host:     host,
-		Database: database,
-		SSLMode:  sslmode,
-		User:     user,
-		Uri:      uri,
-	}, nil
-}
 
-func ConnectPostgres(p *PostgresConn) (*pgx.Conn, error) {
-	if p.Uri == "" {
-		p.Uri = fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s", p.User, p.Password, p.Host, p.Port, p.Database, p.SSLMode)
+	if uri == "" {
+		uri = fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s", user, password, host, portInt, database, sslmode)
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	pgx, err := pgx.Connect(ctx, p.Uri)
+	pgx, err := pgx.Connect(ctx, uri)
 
 	if err != nil {
 		log.Printf("Database connection failed: %v", err)
@@ -55,5 +43,16 @@ func ConnectPostgres(p *PostgresConn) (*pgx.Conn, error) {
 		return nil, err
 	}
 	log.Println("Database connected successfully")
-	return pgx, nil
+
+	conn := NewPostgresConn(pgx)
+
+	err = conn.Create()
+
+	if err != nil {
+		log.Printf("unable to create table: %v", err)
+		return nil, err
+	}
+
+	log.Println("task table create successfully")
+	return conn, nil
 }
--- repository/postgres/create.go
+++ repository/postgres/create.go
@@ -1,11 +1,24 @@
 package postgres
 
+import (
+	"context"
+	"time"
+)
 
-
-func (p *PostgresConn) Create() {
+func (p *PostgresConn) Create() error {
 	query := `
-	CREATE tasks IF NOT EXISTS (
-	
+		CREATE TABLE IF NOT EXISTS tasks (
+			id TEXT PRIMARY KEY,
+			name TEXT,
+			status VARCHAR(100) DEFAULT 'pending',
+			description TEXT,
+			expires_at TIMESTAMP NOT NULL,
+			created_at TIMESTAMP DEFAULT NOW(),
+			updated_at TIMESTAMP DEFAULT NOW()
 	)	
 	`
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	_, err := p.Conn.Exec(ctx, query)
+	return err
 }
