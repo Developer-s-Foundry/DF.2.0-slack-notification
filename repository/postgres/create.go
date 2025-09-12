ec03f945bd49432d8f5e2b8bf375e4716becc0c7
Cu pid <kelanidarasimi9@gmail.com>
2025-09-12 06:28:56 +0100

    chore: add field to table creation

--- repository/postgres/create.go
+++ repository/postgres/create.go
@@ -5,13 +5,25 @@ import (
 	"time"
 )
 
+type Task struct {
+	ID          string    `json:"id"`
+	Name        string    `json:"name"`
+	Status      string    `json:"status"`
+	Description string    `json:"description,omitempty"`
+	AssignedTo  string    `json:"assigned_to,omitempty"`
+	Expires_at  time.Time `json:"expires_at,omitempty"`
+	CreatedAt   time.Time `json:"created_at,omitempty"`
+	UpdatedAt   time.Time `json:"updated_at,omitempty"`
+}
+
 func (p *PostgresConn) Create() error {
 	query := `
 		CREATE TABLE IF NOT EXISTS tasks (
 			id TEXT PRIMARY KEY,
-			name TEXT,
-			status VARCHAR(100) DEFAULT 'pending',
-			description TEXT,
+			name VARCHAR(100) NOT NULL,
+			status VARCHAR(100) NOT NULL DEFAULT 'pending',
+			description TEXT NOT NULL,
+			assigned_to VARCHAR(100),
 			expires_at TIMESTAMP NOT NULL,
 			created_at TIMESTAMP DEFAULT NOW(),
 			updated_at TIMESTAMP DEFAULT NOW()
---
+++ repository/seed/seed_data.go
@@ -0,0 +1,6 @@
+package seed
+
+
+func Seed() {
+
+}
